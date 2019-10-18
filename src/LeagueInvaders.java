938e233808fed88dc1330a195428ac56579efcfd
league <league@imac-27.attlocal.net>
2019-10-17 19:58:15 -0700

    League Invaders IP

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	final int x = 500;
+	final int y = 800;
+	public static void main(String[] args) {
+	
+		
+		
 }
+	LeagueInvaders(){
+	frame = new JFrame();
+	}
+	}
