0fd530699a6e88aa8d7012ea40939f85bbf272c7
league <league@imac-10.attlocal.net>
2020-01-30 19:55:54 -0800

    League Invaders IP

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,38 +1,10 @@
-import java.awt.Dimension;
-import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-
 
 public class LeagueInvaders {
 	JFrame frame;
-	final int x = 500;
-	final int y = 800;
-	GamePanel gPane;
-	public static void main(String[] args) {
-	LeagueInvaders invader = new LeagueInvaders();
-
-		
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+public static void main(String[] args) {
+	
 }
-
-	
-	
-	void setup() {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(gPane);
-		frame.getContentPane().setPreferredSize(new Dimension(x, y));
-        frame.pack();
-       
-	gPane.startGame();
-	}
-	
-	LeagueInvaders(){
-	frame = new JFrame();
-	gPane = new GamePanel();
-	frame.setVisible(true);
-	setup();	
-	}
-	}
-
-
-	
+LeagueInvaders(){}
+}
