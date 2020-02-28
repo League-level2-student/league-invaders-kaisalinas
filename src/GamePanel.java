85b71b89172f4db0268e8ffb914d4eab998af7f8
league <league@imac-38.attlocal.net>
2020-02-27 19:56:01 -0800

    GamePanel IP

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,47 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
-	
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	Font titleFont;
+	int currentState = MENU;
 	GamePanel gPanel;
 	@Override
 	public void paintComponent(Graphics g){
-		g.fillRect(10, 10, 100, 100);
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+		    drawEndState(g);
+		}
 	}
+void updateMenuState(){};
+void updateGameState(){};
+void updateEndState() {};
+void drawMenuState(Graphics g) {
+	g.setColor(Color.BLUE);
+	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	g.setFont(titleFont);
+	g.setColor(Color.YELLOW);
+	g.drawString("League Invaders", 60, 100);
+	
+};
+void drawGameState(Graphics g) {
+	g.setColor(Color.BLACK);
+	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+};
+void drawEndState(Graphics g) {
+	g.setColor(Color.RED);
+	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 }
+GamePanel(){
+	titleFont = new Font("Arial", Font.PLAIN, 48);
+}
+} 
 
