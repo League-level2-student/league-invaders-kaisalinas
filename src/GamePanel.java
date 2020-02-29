4918fdbf35b981c1178b9a7f37e888b9da96f49a
league <league@imac-27.attlocal.net>
2020-02-29 11:25:02 -0800

    GamePanel IP

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,21 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.Timer;
 
 import javax.swing.JPanel;
-
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	final int MENU = 0;
 	final int GAME = 1;
 	final int END = 2;
 	Font titleFont;
+	Font titleFont2;
+	Timer frameDraw;
 	int currentState = MENU;
 	GamePanel gPanel;
 	@Override
@@ -30,7 +37,9 @@ void drawMenuState(Graphics g) {
 	g.setFont(titleFont);
 	g.setColor(Color.YELLOW);
 	g.drawString("League Invaders", 60, 100);
-	
+	g.setFont(titleFont2);
+	g.drawString("Press ENTER to start", 120, 300);
+	g.drawString("Press SPACE for instructions", 80, 450);
 };
 void drawGameState(Graphics g) {
 	g.setColor(Color.BLACK);
@@ -42,6 +51,43 @@ void drawEndState(Graphics g) {
 }
 GamePanel(){
 	titleFont = new Font("Arial", Font.PLAIN, 48);
+	titleFont2 = new Font("Arial", Font.PLAIN, 24);
+	frameDraw = new Timer(1000/60, this);
+	frameDraw.start();
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(currentState == MENU){
+	    updateMenuState();
+	}else if(currentState == GAME){
+	    updateGameState();
+	}else if(currentState == END){
+	    updateEndState();
+	}
+System.out.println("action");
+repaint();
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+	    if (currentState == END) {
+	        currentState = MENU;
+	    } else {
+	        currentState++;
+	    }
+	}   
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 } 
 
