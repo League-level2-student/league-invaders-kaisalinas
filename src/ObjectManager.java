915bd352e123b707d9fa7a27dc008de036d1f76b
root <root@49972c452165>
2021-01-07 03:58:10 +0000

    ObjectManager IP

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class ObjectManager {
+private static final String Graphics = null;
 RocketShip r;
 ArrayList<Projectile> bullets;
 ArrayList<Alien> aliens;
@@ -24,12 +25,26 @@ void update(){
 	if(LeagueInvaders.HEIGHT>800) {
 }
 	}
-void draw() {
-
+void draw(Graphics g) {
+for(int i=0; i<aliens.size(); i++) {
+Rocketship(g);
+}
 }
 
 	
+private void Rocketship(java.awt.Graphics g) {
+	// TODO Auto-generated method stub
+	RocketShip ship = new RocketShip(200, 700, 50, 50, 5, true);
+}
 void purgeObjects() {
+for(int i=0; i<aliens.size();i++) {
+	get(aliens);
 	
 }
+}
+private void get(ArrayList<Alien> aliens2) {
+	// TODO Auto-generated method stub
+	
+}
+
 }
